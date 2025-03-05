8d2df6a7cfdd55165948db51c3617b1f295c45db
Caleb <clebamail@gmail.com>
2025-03-04 19:07:32 -0600

    Implemented local DB and refactored code, .gitignore, and other files

--- local_llama_example.py
+++
@@ -1,7 +0,0 @@
-from langchain_ollama import OllamaLLM #pip install -U langchain-ollama
-
-llm = OllamaLLM(model="codellama:7b")  # Use the correct model name
-prompt = "tell a funny joke"
-
-for chunk in llm.stream(prompt):
-    print(chunk.content if hasattr(chunk, "content") else chunk, end="")
